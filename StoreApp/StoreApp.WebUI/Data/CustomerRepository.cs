c033ec05c5f83855b1403c0c84144ae6db2228cc
Andrew Thiele <308butterfly@gmail.com>
2021-03-03 07:51:36 -0600

    started on customer controller

--- StoreApp/StoreApp.WebUI/Data/CustomerRepository.cs
+++ StoreApp/StoreApp.WebUI/Data/CustomerRepository.cs
@@ -1,11 +1,15 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using StoreApp.WebUI.Models;
-// using StoreApp.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StoreApp.WebUI.Models;
+using StoreApp.Library.Interfaces;
 
-// namespace StoreApp.WebUI.Data
-// {
-// }
+namespace StoreApp.WebUI.Data
+{
+  public class CustomerRepository
+  {
+    private readonly List<Customer> _customerRepository;
+  }
+}
 
