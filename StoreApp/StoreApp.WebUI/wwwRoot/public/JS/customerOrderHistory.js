96230de05f72ba1dbd34c1ad0a9fd110f5ca0521
Andrew Thiele <308butterfly@gmail.com>
2021-03-06 17:26:05 -0600

    fixed link to css style sheet

--- StoreApp/StoreApp.WebUI/wwwRoot/public/JS/customerOrderHistory.js
+++ StoreApp/StoreApp.WebUI/wwwRoot/public/JS/customerOrderHistory.js
@@ -1,14 +1,28 @@
 'use strict';
-
+// HeyYouGuys('HEY YOU GUYS!!!!');
 const anchor = document.getElementById('customerList');
+
+const GET_CUSTOMER_ORDER_DATA = document.getElementById("actionButton");
 
 
 
 LoadCustomerList().then(customers => {
-  debugger;
   for (const customer of customers) {
     anchor.appendChild(new Option(`${customer.firstName} ${customer.lastName}`,`${customer.id}`));
-
   }
 
+  return;
+
 });
+
+GET_CUSTOMER_ORDER_DATA.addEventListener("click", () => {
+  const whichCustomerID = document.getElementById("customerList").value;
+
+  // HeyYouGuys(whichCustomerID);
+
+  const DISPLAY_AREA = document.getElementById("Table Here");
+
+  DISPLAY_AREA.innerHTML = `<p>${whichCustomerID}</p>`;
+});
+
+
