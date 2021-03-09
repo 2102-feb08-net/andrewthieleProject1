81964c78fd030aea8d29506a23543ffc068c6901
Andrew Thiele <308butterfly@gmail.com>
2021-03-08 23:16:37 -0600

    Added logging SUPER THANK YOU TREVOR VIA BRYSON

--- StoreApp/StoreApp.WebUI/Program.cs
+++ StoreApp/StoreApp.WebUI/Program.cs
@@ -6,21 +6,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace StoreApp.WebUI
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      CreateHostBuilder(args).Build().Run();
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                  logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                  logging.AddConsole();
+                  logging.AddDebug();
+                  logging.AddEventSourceLogger();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                  webBuilder.UseStartup<Startup>();
                 });
-    }
+  }
 }
