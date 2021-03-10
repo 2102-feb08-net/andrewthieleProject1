10132b0b9903e5ff9e7012478fde99776cb2a737
Andrew Thiele <308butterfly@gmail.com>
2021-03-10 01:47:08 -0600

    Revert "Added logging SUPER THANK YOU TREVOR VIA BRYSON"
    This reverts commit 81964c78fd030aea8d29506a23543ffc068c6901.

--- StoreApp/StoreApp.WebUI/Program.cs
+++ StoreApp/StoreApp.WebUI/Program.cs
@@ -6,29 +6,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace StoreApp.WebUI
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                  logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                  logging.AddConsole();
-                  logging.AddDebug();
-                  logging.AddEventSourceLogger();
-                })
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                  webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();
                 });
-  }
+    }
 }
